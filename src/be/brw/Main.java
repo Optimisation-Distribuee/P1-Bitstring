e89e4b2d42537a415d3af42df890a17e15f64156
RocherDouteux <f.willems42@gmail.com>
2025-09-25 14:55:17 +0200

    Main
    Add code to bootstrap the algorithm in Main.java

--- src/be/brw/Main.java
+++ src/be/brw/Main.java
@@ -1,7 +1,10 @@
 package be.brw;
 
+import be.brw.domain.SimpleGeneticAlgorithm;
+
 public class Main {
     public static void main(String[] args) {
-
+        SimpleGeneticAlgorithm algorithm = new SimpleGeneticAlgorithm();
+        algorithm.runAlgorithm(10, "0000000000000000000000000000000000000000000000000000000000000001");
     }
 }
