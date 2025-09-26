5bd8f04f40660ca26d1b87d6b2b31d643a326c33
RocherDouteux <f.willems42@gmail.com>
2025-09-26 17:27:13 +0200

    Fix main signature

--- src/be/brw/Main.java
+++ src/be/brw/Main.java
@@ -3,7 +3,7 @@ package be.brw;
 import be.brw.domain.SimpleGeneticAlgorithm;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         SimpleGeneticAlgorithm algorithm = new SimpleGeneticAlgorithm();
         if(!algorithm.runAlgorithm(10, "0000000000000000000000000000000000000000000000000000000000000001")){
             System.out.println("No solution found!");
